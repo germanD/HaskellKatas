ace1ac535be31d5777eeebe07f2a841842a93d70
Germán Delbianco <gad@irif.fr>
2019-08-18 16:30:42 +0200

    WIP: Scoot Encoding Kata

---
+++ src/scott.hs
@@ -0,0 +1,210 @@
+{- Scott Encoding Katta by xenoexplorator -}
+{- https://www.codewars.com/kata/scott-encoding/train/haskell -}
+
+{- Original Description
+
+Can you make algebraic data types even more functional? Of course!
+
+Scott encoding represents data as funtion that apply their argument
+(also a function) to their value. This approach is similar to using
+pattern matching on regular ADTs to extract and use their content.
+
+You are given types representing the Scott encoding of four common
+Haskell types: Maybe, lists, Either and pairs (2-tuples).
+
+Your task is to implement conversion functions between these regular
+types and their Scott encoding.
+
+In addition, you will have to implement the following common
+operations using the provided Scott-encoded data types:
+
+fst and snd, functions to extract the content of a pair
+
+swap, a function that exchanges the content of a pair
+
+curry, a function to turn functions of pairs into functions of two
+arguments
+
+uncurry, a function to turn functions of two arguments into functions
+of pairs
+
+isJust and isNothing, predicates testing wether a Maybe contains data
+
+isLeft and isRight, predicates testing which side is contained in an
+Either
+
+cons, a function to prepend an element to a list
+
+concat, a function to contanetate two lists
+
+catMaybes, a function to flatten a list of Maybes by removing Nothings
+
+null, a predicate testing wether a list is empty
+
+length, a function returning the number of elements in a list
+
+map, a function to transform the contents of a list according to a
+given function
+
+zip, a funtion to merge two lists into a list of pairs
+
+partition, a function that splits a list of Eithers in a pair of Lefts
+and Rights
+
+foldl and foldr, functions to reduce a list to a single value by
+successive application of a given function
+
+take, a function to limit a list to a number of initial elements
+
+-}
+
+
+{-# LANGUAGE ScopedTypeVariables, Rank2Types #-}
+
+module ScottEncoding where
+
+import Prelude hiding (null, length, map, foldl, foldr, take,
+  fst, snd, curry, uncurry, concat, zip, (++))
+
+newtype SPair a b = SPair { runPair :: forall c. (a -> b -> c) -> c }
+
+toPair :: SPair a b -> (a,b)
+toPair m = runPair m (,)
+
+fromPair :: (a,b) -> SPair a b
+fromPair (x,y) = SPair $ \ f -> f x y
+
+-- uncurry (a.k.a fold/split for SPairs) is a natural transformation from
+-- (,)-algebras to SPair-algebras
+
+uncurry :: (a -> b -> c) -> SPair a b -> c
+uncurry f p = runPair p f
+
+fst :: SPair a b -> a
+fst  = uncurry (\ x y -> x)
+
+snd :: SPair a b -> b
+snd  = uncurry (\ x y -> y)
+
+-- curry transforms SPair algebras into (,) algebras
+
+curry :: (SPair a b -> c) -> (a -> b -> c)
+curry f x y = f (fromPair (x,y))
+
+pmap :: (a -> c) -> (b -> d) -> SPair a b -> SPair c d
+pmap  f g = uncurry $ \ a b -> fromPair (f a, g b)
+
+swap :: SPair a b -> SPair b a
+swap  = uncurry $ \ a b -> fromPair (b, a)
+
+
+newtype SMaybe a = SMaybe { runMaybe :: forall b. b -> (a -> b) -> b }
+
+caseM :: b -> (a -> b) -> SMaybe a -> b
+caseM e f m = runMaybe m e f
+
+toMaybe :: SMaybe a -> Maybe a
+toMaybe =  caseM Nothing Just
+
+nothing :: SMaybe a
+nothing = SMaybe $ const
+
+just  :: a -> SMaybe a
+just  = \ x -> SMaybe $ \ _ f -> f x
+
+fromMaybe :: Maybe a -> SMaybe a
+fromMaybe Nothing = nothing
+fromMaybe (Just n) = just n
+
+isJust :: SMaybe a -> Bool
+isJust =  caseM False (const True)
+
+isNothing :: SMaybe a -> Bool
+isNothing = caseM True (const False)
+
+
+newtype SEither a b = SEither { runEither :: forall c. (a -> c) -> (b -> c) -> c }
+
+toEither :: SEither a b -> Either a b
+toEither e = runEither e Left Right
+
+fromEither :: Either a b -> SEither a b
+fromEither (Left a)  = SEither $ \ f _ -> f a 
+fromEither (Right b) = SEither $ \ _ g -> g b 
+
+isLeft :: SEither a b -> Bool
+isLeft e = runEither e (const True) (const False)
+
+isRight :: SEither a b -> Bool
+isRight e = runEither e (const False) (const True) 
+
+caseE :: (a -> c) -> (b -> c) -> SEither a b -> c
+caseE f g e = runEither e f g
+
+newtype SList a = SList { runList :: forall b. b -> (a -> SList a -> b) -> b }
+
+toList :: SList a -> [a]
+toList xs = runList xs [] (\ x ms -> x : toList ms)
+
+fromList :: [a] -> SList a
+fromList [] = SList $ const
+fromList (x:xs) = SList $ \ _ f -> f x $ fromList xs
+
+cons :: a -> SList a -> SList a
+cons x ls = SList $ \ _ f -> f x ls
+
+nil :: SList a
+nil = SList $ \ e _ -> e
+
+null :: SList a -> Bool
+null xs = runList xs True $ \ _ _ -> False
+
+foldr :: (a -> b -> b) -> b -> SList a -> b
+foldr f e l = runList l e $ \ x m -> f x $ foldr f e m
+
+foldl :: (b -> a -> b) -> b -> SList a -> b
+foldl f e l = runList l e $ \ x  -> foldl f $ f e x
+
+map :: (a -> b) -> SList a -> SList b
+map f = foldr ( \ x ms -> cons (f x) ms) nil
+
+
+-- List Surgery
+
+length :: SList a -> Int
+length = foldr (const (+1)) 0
+
+concat :: SList a -> SList a -> SList a
+concat = foldr (\ x g -> (cons x) . g) id
+
+rcons :: SList a -> a -> SList a
+rcons xs = concat xs . (flip cons $ nil)  
+
+take :: Int -> SList a -> SList a
+take n xs = foldr falg (const nil) xs n
+ where falg x rs n 
+         | n <= 0    = nil
+         | otherwise = cons x $ rs $ n -1 
+
+-- TODO: Scott encoding for Nats 
+
+catMaybes :: SList (SMaybe a) -> SList a
+catMaybes = foldr falg nil
+ where falg x ls = concat (caseM nil (flip cons $ nil) x) ls
+
+--- zip"
+
+zip :: SList a -> SList b -> SList (SPair a b)
+zip = error "zip"
+
+partition :: SList (SEither a b) -> SPair (SList a) (SList b)
+partition = foldr falg $ fromPair (nil :: SList a, nil :: SList b)
+  where falg ab ms = let ls = fst ms
+                         rs = snd ms
+                     in caseE (\ x -> fromPair (cons x ls,rs))
+                              (\ x -> fromPair (ls, cons x rs)) ab
+
+
+
+
+
---
+++ test/scott.test.hs
@@ -0,0 +1,114 @@
+module ScottEncoding.Test where
+
+import Prelude hiding (null, length, map, foldl, foldr, take, fst, 
+                        snd, curry, uncurry, concat, zip, (++), filter)
+import ScottEncoding
+import Test.Hspec
+
+reduce :: Num a => a -> SList a -> a
+reduce i sl = i + 10 * runList sl 0 reduce
+
+testMB =   
+  describe "The Maybe type" $ do
+    it "can be cast to Prelude.Maybe" $ do
+      toMaybe (SMaybe const) `shouldBe` (Nothing :: Maybe Int)
+      toMaybe (SMaybe $ \_ f -> f 4) `shouldBe` Just 4
+    it "can be cast from Prelude.Maybe" $ do
+      runMaybe (fromMaybe (Nothing)) 0 (+1) `shouldBe` 0
+      runMaybe (fromMaybe (Just 4)) 0 (+1) `shouldBe` 5
+    it "isJust/isNothing" $ do
+      isJust (fromMaybe (Nothing :: Maybe Int)) `shouldBe` False
+      isJust (fromMaybe (Just 4)) `shouldBe` True
+      
+testP = 
+  describe "The pair type" $ do
+    it "can be cast to (,)" $ do
+      toPair (SPair $ \f -> f 2 "hi") `shouldBe` (2, "hi")
+    it "can be cast from (,)" $ do
+      runPair (fromPair (2, "hi")) replicate `shouldBe` ["hi", "hi"]      
+    it "swap (a,b) = (b,a) " $ do
+      snd (swap (fromPair (3, 2))) `shouldBe` 3
+      fst (swap (fromPair (3, 2))) `shouldBe` 2
+    it " curry / uncurry  " $ do
+      uncurry (+) (fromPair (3, 2)) `shouldBe` 5
+      curry ( \ p -> fst p + snd p) 3 2 `shouldBe` 5
+
+testE =
+    describe "The Either type" $ do
+      it "can be cast to Prelude.Either" $ do
+        toEither (SEither $ \f _ -> f 3) `shouldBe` (Left 3 :: Either Int String)
+        toEither (SEither $ \_ f -> f "hello") `shouldBe` (Right "hello" :: Either Int String)
+      it "can be cast from Prelude.Either" $ do
+        runEither (fromEither (Left 3)) show id `shouldBe` "3"
+        runEither (fromEither (Right "hello" :: Either Int String)) show id `shouldBe` "hello"
+      it "Left/Right" $ do
+        isLeft (fromEither (Left 3))  `shouldBe` True
+        isLeft (fromEither (Right 3))  `shouldBe` False
+        isRight (fromEither (Left 3))  `shouldBe` False
+        isRight (fromEither (Right 3))  `shouldBe` True
+
+testL =
+    describe "The list type" $ do
+      it "can be cast to []" $ do
+          toList (SList const) `shouldBe` ([] :: [Int])
+          toList (SList $ \_ f -> f 1 (SList $ \_ g -> g 2 (SList const))) `shouldBe` [1,2]
+      it "can be cast from []" $ do
+          runList (fromList []) 0 reduce `shouldBe` 0
+          runList (fromList [1,2]) 0 reduce `shouldBe` 21
+      it "null xs?" $ do
+          null  nil `shouldBe` True
+          null (fromList []) `shouldBe` True
+          null (fromList [1,2]) `shouldBe` False
+      it "cons Tests " $ do
+          toList (cons 1 (fromList [])) `shouldBe` [1]
+          null (cons 7 (fromList [2..5])) `shouldBe` False
+
+testF = 
+    describe "Fold/Map Properties for List" $ do
+      it "foldr cons nil = id" $ do
+        (toList $ foldr cons nil $ fromList [1..5]) `shouldBe` [1..5]
+      it "foldl (flip cons) nil = rev" $ do
+        (toList $ foldl (flip cons) nil $ fromList [1..5]) `shouldBe` (reverse [1..5])
+      it "foldl rcons nil = id" $ do
+        (toList $ foldl rcons nil $ fromList [1..5]) `shouldBe` [1..5]
+      it "Map Functor: map id = id" $ do
+        (toList $ map id $ fromList [1..5]) `shouldBe` [1..5]
+      it "Map Functor: map assoc" $ do
+        (toList $ map (+1) $ map (+3) $ fromList [1..5]) `shouldBe` 
+          (toList $ map (+4) $ fromList [1..5])    
+testD = 
+     describe "Distributive Laws for Lists" $ do
+      it "Concat Map : map f (concat l r) = concat (map f l) (map f r)" $ do
+          (toList $ map (+1) $ concat (fromList [1..5]) (fromList [6..10])) `shouldBe` 
+            (toList $ concat (map (+1) $ fromList [1..5]) (map (+1) $ fromList [6..10]))
+testS = 
+     describe "List Surgery" $ do
+      it "take Basics" $ do
+          (toList $ take 8 nil) `shouldBe` ([] :: [Int])
+          (toList $ take 0 $ fromList [1..5]) `shouldBe` []
+          (toList $ take 5 $ fromList [1..8]) `shouldBe` [1..5]
+      it "take lenght = id" $ do
+           let ms = fromList [1..5]
+           (toList $ take (length ms) ms) `shouldBe` [1..5]
+      it "concat Surgery" $ do
+           let ls = fromList [1..5]
+               rs = fromList [6..10]
+            in (toList $ concat ls rs) `shouldBe` [1..10]           
+      it "catMaybe . map Just = id" $ do
+           let ms = fromList [1..5]
+           (toList $ catMaybes $ map just ms) `shouldBe` [1..5]
+      it "catMaybe Surgery" $ do
+           let ls = map just $ fromList [1..5]
+               rs = map just $ fromList [6..10]
+               xs = concat ls (cons nothing rs)
+            in (toList $ catMaybes xs) `shouldBe` [1..10]
+      
+main = 
+  do hspec testP
+     hspec testMB
+     hspec testE
+     hspec testL
+     hspec testF
+     hspec testD
+     hspec testS
+ 
